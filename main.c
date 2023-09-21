3af56e8c80a3ac867903f73c3f377768b2dcd765
Rooibos15 <15eal.rb@gmail.com>
2023-09-21 16:38:41 +0900

    5

--- main.c
+++ main.c
@@ -4,13 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
+	int a, b;
 	
-	printf("input a chracter : ");
-	scanf("%c", &c);
 	
-	printf("the next character of %c (%d) is  %c(%d)\n", c, c, c+1, c+1 );
+	printf("분자를 입력하세요 : ");
+	scanf("%d", &a);
 	
+	printf("분모를 입력하세요 : ");
+	scanf("%d", &b);
+	
+	printf("나누기의 결과는 %f\n", (float)a/b);
+
 	system("PAUSE");
 	return 0;
 }
